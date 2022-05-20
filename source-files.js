365d9aa013c363622155ddf7ecd7f27938dad118
zhenfeizhang <zhenfeizhang@users.noreply.github.com>
2022-05-20 20:34:38 +0000

    deploy: 3ea5b61c5e713282cb983b52804a0c83f549cbd7

--- source-files.js
+++ source-files.js
@@ -1,5 +1,5 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["hyperplonk"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["pcs"] = {"name":"","files":["commit.rs","errors.rs","lib.rs","param.rs"]};
-sourcesIndex["poly_iop"] = {"name":"","dirs":[{"name":"sum_check","files":["mod.rs","prover.rs","verifier.rs"]},{"name":"zero_check","files":["mod.rs"]}],"files":["errors.rs","lib.rs","structs.rs","transcript.rs","utils.rs","virtual_poly.rs"]};
+sourcesIndex["poly_iop"] = {"name":"","dirs":[{"name":"perm_check","files":["mod.rs"]},{"name":"sum_check","files":["mod.rs","prover.rs","verifier.rs"]},{"name":"zero_check","files":["mod.rs"]}],"files":["errors.rs","lib.rs","structs.rs","transcript.rs","utils.rs","virtual_poly.rs"]};
 createSourceSidebar();
