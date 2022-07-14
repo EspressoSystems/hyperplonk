39b81e59aeffea1a6974b37afae95d724ec75979
chancharles92 <chancharles92@users.noreply.github.com>
2022-07-14 19:24:57 +0000

    deploy: 43131a0afbad775c42da874cf2aac6774e5cae7d

--- source-files.js
+++ source-files.js
@@ -1,5 +1,5 @@
 var sourcesIndex = {};
 sourcesIndex["hyperplonk"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["pcs"] = {"name":"","files":["commit.rs","errors.rs","lib.rs","param.rs","util.rs"]};
-sourcesIndex["poly_iop"] = {"name":"","dirs":[{"name":"hyperplonk","files":["mod.rs"]},{"name":"perm_check","files":["mod.rs","util.rs"]},{"name":"sum_check","files":["mod.rs","prover.rs","verifier.rs"]},{"name":"zero_check","files":["mod.rs"]}],"files":["errors.rs","lib.rs","structs.rs","transcript.rs","utils.rs","virtual_poly.rs"]};
+sourcesIndex["poly_iop"] = {"name":"","dirs":[{"name":"hyperplonk","files":["mod.rs"]},{"name":"perm_check","files":["mod.rs","util.rs"]},{"name":"prod_check","files":["mod.rs"]},{"name":"sum_check","files":["mod.rs","prover.rs","verifier.rs"]},{"name":"zero_check","files":["mod.rs"]}],"files":["errors.rs","lib.rs","structs.rs","transcript.rs","utils.rs","virtual_poly.rs"]};
 createSourceSidebar();
