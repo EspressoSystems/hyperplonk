de4e2731cad3da61dc86f35336675c3c050a5daf
chancharles92 <chancharles92@users.noreply.github.com>
2022-08-12 18:43:11 +0000

    deploy: 8281e7c8779bb0d1b0f8d77aa520076e1f6acd4b

--- hyperplonk/sidebar-items.js
+++ hyperplonk/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"macro":[["build_mle","Build MLE from matrix of witnesses."]],"trait":[["HyperPlonkSNARK","A trait for HyperPlonk Poly-IOPs. A HyperPlonk is derived from SumChecks, ZeroChecks and PermutationChecks."]]});
+window.SIDEBAR_ITEMS = {"macro":[["build_mle","Build MLE from matrix of witnesses."]],"trait":[["HyperPlonkSNARK","A trait for HyperPlonk Poly-IOPs. A HyperPlonk is derived from SumChecks, ZeroChecks and PermutationChecks."]]};
