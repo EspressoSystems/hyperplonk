c4e023a75841ea6d78d88dcd91bcb2dbc10d7720
zhenfei <zhenfei.zhang@hotmail.com>
2022-09-19 21:07:48 -0400

    Update new.rs

--- pcs/src/multilinear_kzg/batching/new.rs
+++ pcs/src/multilinear_kzg/batching/new.rs
@@ -3,20 +3,21 @@
 ///
 ///
 /// steps:
-/// 1. for `i in [0..k-1]`, set `L_i(x) = LI(p[i])`, where `LI` is Lagrange
-/// interpolation of p[i].
-/// 2. define `g(y, x0,...x_{n-1}) := \sum_{i=0}^{k-1} LI(y) f_i` which is an
-/// (n+1) mle
-/// 3. define `h(y) := \sum_{i=0}^{k-1} LI(y) p_i` which are n
+/// 2. define `g(y, x0,...x_{n-1}) := \sum_{i=0}^{k-1} L_i(y) f_i` which is an
+/// (n+1) mle (L_i(y) is implicit)
+/// 3. define `h(y) := \sum_{i=0}^{k-1} L_i(y) p_i` which are n (h(y) is implicit)
 /// univariate polynomials
 /// 4. evaluate `q(y) := g(y, h1, ...hn)` and obtain a univariate polynomial
-/// this is done via `compute_w_circ_l` api
+/// this is done via ~~`compute_w_circ_l` api~~ interpolation
+///   4.1 y in [w..w^k] -> n evalation: y_1...y_k
+///   4.2 evaluate y at alpha...alpha (d* mu +1 )(k-1) - k
+///   4.3 interpolate to get `q(y)` explict form
 /// 5. commit to `q(y)` and append commitment to transcript
 ///
 /// 6. sample a random field element `r` from transcript
-/// 7. generate `g(r, x0,...x_{n-1}) := \sum_{i=0}^{k-1} LI(r) f_i` which is an
+/// 7. generate `g(r, x0,...x_{n-1}) := \sum_{i=0}^{k-1} L_i(r) f_i` which is an
 /// `n` dim MLE
-/// 8. compute `h(r) := \sum_{i=0}^{k-1} LI(r) p_i` which is an `n` dim point
+/// 8. compute `h(r) := \sum_{i=0}^{k-1} L_i(r) p_i` which is an `n` dim point
 ///
 /// 9. open `q(y)` at `r` and outputs its evaluation and proof
 /// 10. open `g(y, x0,...x_{n-1})` at `r, h1(r),...hn(r)` and outputs its
