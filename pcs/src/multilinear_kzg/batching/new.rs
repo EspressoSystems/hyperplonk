21dfc64b688330784f5b18eb3f9835e45aca47e5
zhenfei <zhenfei.zhang@hotmail.com>
2022-09-20 15:56:23 -0400

    skeleton of batch proving

--- pcs/src/multilinear_kzg/batching/new.rs
+++ pcs/src/multilinear_kzg/batching/new.rs
@@ -1,7 +1,10 @@
 use crate::{
-    multilinear_kzg::{srs::MultilinearProverParam, util::get_uni_domain},
+    multilinear_kzg::{
+        open_internal, srs::MultilinearProverParam, util::get_uni_domain, MultilinearKzgPCS,
+    },
     prelude::PCSError,
-    univariate_kzg::srs::UnivariateProverParam,
+    univariate_kzg::{srs::UnivariateProverParam, UnivariateKzgPCS},
+    PolynomialCommitmentScheme,
 };
 use arithmetic::DenseMultilinearExtension;
 use ark_ec::PairingEngine;
@@ -12,6 +15,7 @@ use ark_poly::{
 };
 use ark_std::{end_timer, start_timer};
 use std::rc::Rc;
+use transcript::IOPTranscript;
 
 /// input: k points `p`, each of dim n
 /// input: k MLEs `f`, each of dim n
@@ -42,7 +46,84 @@ fn open_new<E: PairingEngine>(
     ml_prover_param: &MultilinearProverParam<E>,
     polynomials: &[Rc<DenseMultilinearExtension<E::Fr>>],
     points: &[Vec<E::Fr>],
-) {
+) -> Result<(), PCSError> {
+    // 3. evaluate `q(y) := g(y, h1, ...hn)` and obtain a univariate polynomial
+    // this is done via interpolation
+    let q_y = compute_q_y(polynomials, points)?;
+    // 4. commit to `q(y)` and append commitment to transcript
+
+    let mut transcript = IOPTranscript::<E::Fr>::new(b"ml kzg");
+    for point in points {
+        // println!("points proving {:?}", points);
+        transcript.append_serializable_element(b"w", point)?;
+    }
+
+    let q_y_commit = UnivariateKzgPCS::<E>::commit(uni_prover_param, &q_y)?;
+    transcript.append_serializable_element(b"q(y)", &q_y_commit)?;
+
+    // 5. sample a random field element `r` from transcript
+    let r = transcript.get_and_append_challenge(b"r")?;
+
+    // 6. generate `g(r, x0,...x_{n-1}) := \sum_{i=0}^{k-1} L_i(r) f_i` which is an
+    // `n` dim MLE
+
+    let g_y = build_g_mle(polynomials, &r)?;
+
+    // 7. compute `h(r) := \sum_{i=0}^{k-1} L_i(r) p_i` which is an `n` dim point
+    let h_r = build_h_r(points, &r)?;
+
+    // 8. open `q(y)` at `r` and outputs its evaluation and proof
+    let (q_r_proof, q_r_eval) = UnivariateKzgPCS::<E>::open(uni_prover_param, &q_y, &r)?;
+
+    // 9. open `g(y, x0,...x_{n-1})` at `r, h1(r),...hn(r)` and outputs its
+    // evaluations and proof
+    let (g_r_proof, g_r_eval) = open_internal(ml_prover_param, &g_y, &h_r)?;
+    Ok(())
+}
+
+/// 7. compute `h(r) := \sum_{i=0}^{k-1} L_i(r) p_i` which is an `n` dim point
+// TODO: merge this step with step 6?
+fn build_h_r<F: PrimeField>(points: &[Vec<F>], r: &F) -> Result<Vec<F>, PCSError> {
+    let k = points.len();
+    let nv = points[0].len();
+    let domain_k: Radix2EvaluationDomain<F> = get_uni_domain(k)?;
+    let l_i_r_eval = domain_k.evaluate_all_lagrange_coefficients(*r);
+    let mut h_r = vec![F::zero(); nv];
+
+    for i in 0..nv {
+        for j in 0..k {
+            h_r[i] += points[j][i] * l_i_r_eval[j];
+        }
+    }
+
+    Ok(h_r)
+}
+
+/// 6. generate `g(r, x0,...x_{n-1}) := \sum_{i=0}^{k-1} L_i(r) f_i` which is an
+/// `n` dim MLE
+fn build_g_mle<F: PrimeField>(
+    polynomials: &[Rc<DenseMultilinearExtension<F>>],
+    r: &F,
+) -> Result<Rc<DenseMultilinearExtension<F>>, PCSError> {
+    let k = polynomials.len();
+    let nv = polynomials[0].num_vars;
+    let domain_k: Radix2EvaluationDomain<F> = get_uni_domain(k)?;
+    let l_i_r_eval = domain_k.evaluate_all_lagrange_coefficients(*r);
+    let mut evaluations = vec![F::zero(); 1 << nv];
+
+    // TODO: optimization
+    for i in 0..1 << nv {
+        for j in 0..k {
+            evaluations[i] += polynomials[j].evaluations[i] * l_i_r_eval[j];
+        }
+    }
+
+    let res = Rc::new(DenseMultilinearExtension::from_evaluations_vec(
+        nv,
+        evaluations,
+    ));
+
+    Ok(res)
 }
 
 /// compute the degree of q(y) which is (k-1)(d*n+1)
@@ -137,7 +218,7 @@ mod test {
     use ark_bls12_381::Fr;
     use ark_ff::PrimeField;
 
-    use super::{compute_q_y, eval_h_y_at_roots};
+    use super::{build_g_mle, build_h_r, compute_q_y, eval_h_y_at_roots};
 
     #[test]
     fn test_compute_q_y() -> Result<(), PCSError> {
@@ -170,8 +251,15 @@ mod test {
         let w_eval = vec![F::zero(), F::one(), F::one(), F::from(2u64)];
         let w2 = Rc::new(DenseMultilinearExtension::from_evaluations_vec(2, w_eval));
 
-        let point = eval_h_y_at_roots(&[point1.clone(), point2.clone()])?;
-        let q_y = compute_q_y(&[w1, w2], &[point1, point2]);
+        let points = &[point1.clone(), point2.clone()];
+        let polynomials = &[w1.clone(), w2.clone()];
+
+        let point = eval_h_y_at_roots(points)?;
+        let q_y = compute_q_y(polynomials, points);
+
+        let r = F::from(10u64);
+        let g_mle = build_g_mle(polynomials, &r)?;
+        let hr = build_h_r(points, &r)?;
 
         Ok(())
     }
