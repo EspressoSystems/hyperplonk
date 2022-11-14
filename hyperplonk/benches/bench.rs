2842e8d4c3fe7d09277f51c654bcc86e64746e03
zhenfei <zhenfei.zhang@hotmail.com>
2022-11-13 21:46:42 -0500

    wip

--- hyperplonk/benches/bench.rs
+++ hyperplonk/benches/bench.rs
@@ -16,27 +16,34 @@ use subroutines::{
     poly_iop::PolyIOP,
 };
 
-const SUPPORTED_SIZE: usize = 25;
+// For 32 G of ram we can do nv = 24
+// For 128 G of ram we can do nv = 26
+const SUPPORTED_SIZE: usize = 26;
 const MIN_NUM_VARS: usize = 8;
-const MAX_NUM_VARS: usize = 22;
+const MAX_NUM_VARS: usize = SUPPORTED_SIZE;
 const MIN_CUSTOM_DEGREE: usize = 1;
 const MAX_CUSTOM_DEGREE: usize = 32;
 
 fn main() -> Result<(), HyperPlonkErrors> {
     let args: Vec<String> = env::args().collect();
-    let thread = args[1].parse().unwrap_or(24);
+    let thread = args[1].parse().unwrap_or(0);
     let mut rng = test_rng();
     let pcs_srs = MultilinearKzgPCS::<Bls12_381>::gen_srs_for_testing(&mut rng, SUPPORTED_SIZE)?;
-    ThreadPoolBuilder::new()
-        .num_threads(thread)
-        .build()
-        .unwrap();
-    bench_jellyfish_plonk(&pcs_srs, thread)?;
-    bench_vanilla_plonk(&pcs_srs, thread)?;
-    for degree in MIN_CUSTOM_DEGREE..MAX_CUSTOM_DEGREE {
-        bench_high_degree_plonk(&pcs_srs, degree, thread)?;
-    }
+    if thread == 0 {
+        let thread = 64;
+        bench_jellyfish_plonk(&pcs_srs, thread)?;
+        bench_vanilla_plonk(&pcs_srs, thread)?;
+        for degree in MIN_CUSTOM_DEGREE..MAX_CUSTOM_DEGREE {
+            bench_high_degree_plonk(&pcs_srs, degree, thread)?;
+        }
+    } else {
+        ThreadPoolBuilder::new()
+            .num_threads(thread)
+            .build()
+            .unwrap();
 
+        bench_jellyfish_plonk(&pcs_srs, thread)?;
+    }
     Ok(())
 }
 
@@ -46,7 +53,7 @@ fn bench_jellyfish_plonk(
 ) -> Result<(), HyperPlonkErrors> {
     let filename = format!("jellyfish threads {}.txt", thread);
     let mut file = File::create(filename).unwrap();
-    for nv in MIN_NUM_VARS..MAX_NUM_VARS {
+    for nv in MIN_NUM_VARS..=MAX_NUM_VARS {
         let vanilla_gate = CustomizedGates::jellyfish_turbo_plonk_gate();
         bench_mock_circuit_zkp_helper(&mut file, nv, &vanilla_gate, &pcs_srs)?;
     }
@@ -60,7 +67,7 @@ fn bench_vanilla_plonk(
 ) -> Result<(), HyperPlonkErrors> {
     let filename = format!("vanilla threads {}.txt", thread);
     let mut file = File::create(filename).unwrap();
-    for nv in MIN_NUM_VARS..MAX_NUM_VARS {
+    for nv in MIN_NUM_VARS..=MAX_NUM_VARS {
         let vanilla_gate = CustomizedGates::vanilla_plonk_gate();
         bench_mock_circuit_zkp_helper(&mut file, nv, &vanilla_gate, &pcs_srs)?;
     }
@@ -75,7 +82,7 @@ fn bench_high_degree_plonk(
 ) -> Result<(), HyperPlonkErrors> {
     let filename = format!("high degree {} thread {}.txt", degree, thread);
     let mut file = File::create(filename).unwrap();
-    for nv in MIN_NUM_VARS..MAX_NUM_VARS {
+    for nv in MIN_NUM_VARS..=MAX_NUM_VARS {
         let vanilla_gate = CustomizedGates::mock_gate(2, degree);
         bench_mock_circuit_zkp_helper(&mut file, nv, &vanilla_gate, &pcs_srs)?;
     }
@@ -142,7 +149,11 @@ fn bench_mock_circuit_zkp_helper(
             )?;
     }
     let t = start.elapsed().as_micros() / repetition as u128;
-
+    println!(
+        "proving for {} variables: {} us",
+        nv,
+        start.elapsed().as_micros() / repetition as u128
+    );
     file.write_all(format!("{} {}\n", nv, t).as_ref()).unwrap();
 
     let proof = <PolyIOP<Fr> as HyperPlonkSNARK<Bls12_381, MultilinearKzgPCS<Bls12_381>>>::prove(
