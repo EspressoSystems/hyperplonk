58daaeb270ff75bbf229957cdcd32423f1de50a0
zhenfeizhang <zhenfeizhang@users.noreply.github.com>
2023-04-03 00:23:06 +0000

    deploy: cb19852e11ab61d481c8322e9bc9d78948bcec80

--- implementors/core/hash/trait.Hash.js
+++ implementors/core/hash/trait.Hash.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"subroutines":[["impl&lt;E:&nbsp;Pairing&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"subroutines/pcs/prelude/struct.Commitment.html\" title=\"struct subroutines::pcs::prelude::Commitment\">Commitment</a>&lt;E&gt;"]]
+"subroutines":[["impl&lt;E:&nbsp;Pairing&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.2/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"subroutines/pcs/prelude/struct.Commitment.html\" title=\"struct subroutines::pcs::prelude::Commitment\">Commitment</a>&lt;E&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
