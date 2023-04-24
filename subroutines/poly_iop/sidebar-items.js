ebbbe3e2b1f85f3ad2db58eb11e9bb1bc9aa97fb
zhenfeizhang <zhenfeizhang@users.noreply.github.com>
2023-04-24 00:22:11 +0000

    deploy: cb19852e11ab61d481c8322e9bc9d78948bcec80

--- subroutines/poly_iop/sidebar-items.js
+++ subroutines/poly_iop/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":[["prelude",""]],"struct":[["PolyIOP","Struct for PolyIOP protocol. It has an associated type `F` that defines the prime field the multi-variate polynomial operates on."]]};
+window.SIDEBAR_ITEMS = {"mod":["prelude"],"struct":["PolyIOP"]};
--- subroutines/sidebar-items.js
+++ subroutines/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"macro":[["to_bytes","Takes as input a struct, and converts them to a series of bytes. All traits that implement `CanonicalSerialize` can be automatically converted to bytes in this manner."]],"mod":[["pcs",""],["poly_iop",""]]};
+window.SIDEBAR_ITEMS = {"macro":["to_bytes"],"mod":["pcs","poly_iop"]};
