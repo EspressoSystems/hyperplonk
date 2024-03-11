cc2abf538663ecdd60f09dc5f35615da99d5d1af
Ancient123 <Ancient123@users.noreply.github.com>
2024-03-11 21:21:36 +0000

    deploy: 3330619ba7c7443e8a708f08f2194bc0f14f7f19

--- implementors/core/ops/arith/trait.Add.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"arithmetic":[["impl&lt;F: PrimeField&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.70.0/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a>&lt;&amp;<a class=\"struct\" href=\"arithmetic/struct.VirtualPolynomial.html\" title=\"struct arithmetic::VirtualPolynomial\">VirtualPolynomial</a>&lt;F&gt;&gt; for &amp;<a class=\"struct\" href=\"arithmetic/struct.VirtualPolynomial.html\" title=\"struct arithmetic::VirtualPolynomial\">VirtualPolynomial</a>&lt;F&gt;"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++
@@ -1,8 +0,0 @@
-var sourcesIndex = JSON.parse('{\
-"arithmetic":["",[],["errors.rs","lib.rs","multilinear_polynomial.rs","univariate_polynomial.rs","util.rs","virtual_polynomial.rs"]],\
-"hyperplonk":["",[],["custom_gate.rs","errors.rs","lib.rs","mock.rs","prelude.rs","selectors.rs","snark.rs","structs.rs","utils.rs","witness.rs"]],\
-"subroutines":["",[["pcs",[["multilinear_kzg",[],["batching.rs","mod.rs","srs.rs","util.rs"]],["univariate_kzg",[],["mod.rs","srs.rs"]]],["errors.rs","mod.rs","prelude.rs","structs.rs"]],["poly_iop",[["perm_check",[],["mod.rs","util.rs"]],["prod_check",[],["mod.rs","util.rs"]],["sum_check",[],["mod.rs","prover.rs","verifier.rs"]],["zero_check",[],["mod.rs"]]],["errors.rs","mod.rs","prelude.rs","structs.rs","utils.rs"]]],["lib.rs"]],\
-"transcript":["",[],["errors.rs","lib.rs"]],\
-"util":["",[],["lib.rs"]]\
-}');
-createSourceSidebar();
