9aa5f21caba4194f1e1a5acc9892578e0f119287
zhenfeizhang <zhenfeizhang@users.noreply.github.com>
2024-08-05 00:19:08 +0000

    deploy: 8698369edfe82bd6617a9609602380f21cabd1da

--- trait.impl/core/ops/arith/trait.Add.js
+++ trait.impl/core/ops/arith/trait.Add.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"arithmetic":[["impl&lt;F: PrimeField&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.79.0/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a> for &amp;<a class=\"struct\" href=\"arithmetic/struct.VirtualPolynomial.html\" title=\"struct arithmetic::VirtualPolynomial\">VirtualPolynomial</a>&lt;F&gt;"]]
+"arithmetic":[["impl&lt;F: PrimeField&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.80.0/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a> for &amp;<a class=\"struct\" href=\"arithmetic/struct.VirtualPolynomial.html\" title=\"struct arithmetic::VirtualPolynomial\">VirtualPolynomial</a>&lt;F&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
