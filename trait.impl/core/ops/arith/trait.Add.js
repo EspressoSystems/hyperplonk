956aec85adfc084d7b161cd21d9f285a13a66eb4
zhenfeizhang <zhenfeizhang@users.noreply.github.com>
2026-03-23 00:33:23 +0000

    deploy: 2a3b55c97ad8a5d6627108a2e7def2aeccb7f3b9

--- trait.impl/core/ops/arith/trait.Add.js
+++ trait.impl/core/ops/arith/trait.Add.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["arithmetic",[["impl&lt;F: PrimeField&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a> for &amp;<a class=\"struct\" href=\"arithmetic/struct.VirtualPolynomial.html\" title=\"struct arithmetic::VirtualPolynomial\">VirtualPolynomial</a>&lt;F&gt;"]]]]);
+    const implementors = Object.fromEntries([["arithmetic",[["impl&lt;F: PrimeField&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a> for &amp;<a class=\"struct\" href=\"arithmetic/struct.VirtualPolynomial.html\" title=\"struct arithmetic::VirtualPolynomial\">VirtualPolynomial</a>&lt;F&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[340]}
+//{"start":59,"fragment_lengths":[342]}
